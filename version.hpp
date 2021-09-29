ac90a3db48ed4955a802de3530278f8fe85655b8
Stefan Reuther <streu@gmx.de>
2021-09-29 18:00:55 +0200

    Version 2.40.11

--- version.hpp
+++ version.hpp
@@ -5,8 +5,8 @@
 #ifndef C2NG_VERSION_HPP
 #define C2NG_VERSION_HPP
 
-#define PCC2_VERSION "2.40.10 (c2ng)"
-#define PCC2_VERSION_CODE 240010
+#define PCC2_VERSION "2.40.11 (c2ng)"
+#define PCC2_VERSION_CODE 240011
 
 #define PCC2_URL     "http://phost.de/~stefan/pcc2.html"
 
