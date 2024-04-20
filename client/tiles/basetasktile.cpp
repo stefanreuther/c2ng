stefanreuther/c2ng

commit 38726fba52b3faccbed87c90f439f0d35b31ba32

Auto Task: add ability to build starbases and planetary buildings
Extend BaseTaskTile to show build orders other than ships, and also use it
to decode commands on Planet Task Screen.

client/tiles/basetasktile.cpp
@@ -12,7 +12,7 @@
 using util::rich::Text;
 using util::SkinColor;
 
-client::tiles::BaseTaskTile::BaseTaskTile(ui::Root& root, gfx::KeyEventConsumer& keyHandler, afl::string::Translator& tx)
+client::tiles::BaseTaskTile::BaseTaskTile(Personality pers, ui::Root& root, gfx::KeyEventConsumer& keyHandler, afl::string::Translator& tx)
     : TaskMessageTile(root, keyHandler, tx),
       m_statusView(gfx::Point(root.provider().getFont(gfx::FontRequest())->getCellSize().scaledBy(15, 7)), 0, root.provider()),
       m_editButton("E", 'e', root)
@@ -26,11 +26,13 @@ client::tiles::BaseTaskTile::BaseTaskTile(ui::Root& root, gfx::KeyEventConsumer&
     addCommandButton('3', tx("3 - Misc."));
     commandPart().add(deleter().addNew(new ui::Spacer()));
 
-    ui::Group& g = deleter().addNew(new ui::Group(ui::layout::HBox::instance5));
-    g.add(deleter().addNew(new ui::widgets::StaticText(tx("Edit"), SkinColor::Static, gfx::FontRequest(), root.provider(), gfx::RightAlign))
-          .setIsFlexible(true));
-    g.add(m_editButton);
-    commandPart().add(g);
+    if (m_personality == BasePersonality) {
+        ui::Group& g = deleter().addNew(new ui::Group(ui::layout::HBox::instance5));
+        g.add(deleter().addNew(new ui::widgets::StaticText(tx("Edit"), SkinColor::Static, gfx::FontRequest(), root.provider(), gfx::RightAlign))
+              .setIsFlexible(true));
+        g.add(m_editButton);
+        commandPart().add(g);
+    }
 
     setBaseStatus(game::proxy::TaskEditorProxy::BaseStatus());
 }
@@ -49,7 +51,7 @@ client::tiles::BaseTaskTile::setBaseStatus(const game::proxy::TaskEditorProxy::B
         afl::string::Translator& tx = translator();
 
         // Render build order
-        doc.add(Text(SkinColor::White, tx("Ship Build Order:")));
+        doc.add(Text(SkinColor::White, tx("Build Order:")));
         doc.addNewline();
         for (size_t i = 0; i < st.buildOrder.size(); ++i) {
             doc.add(pfx + st.buildOrder[i]);
@@ -69,5 +71,5 @@ client::tiles::BaseTaskTile::setBaseStatus(const game::proxy::TaskEditorProxy::B
     doc.finish();
     m_statusView.handleDocumentUpdate();
 
-    m_editButton.setState(DisabledState, st.buildOrder.empty());
+    m_editButton.setState(DisabledState, st.buildOrder.empty() || !st.isShipBuildOrder);
 }

client/tiles/basetasktile.hpp
@@ -24,11 +24,17 @@ namespace client { namespace tiles {
         - connect TaskEditorProxy::sig_messageChange to setMessageStatus */
     class BaseTaskTile : public TaskMessageTile {
      public:
+        enum Personality {
+            PlanetPersonality,
+            BasePersonality
+        };
+
         /** Constructor.
+            @param pers        Personality
             @param root        UI root
             @param keyHandler  Key handler to process button events
             @param tx          Translator */
-        BaseTaskTile(ui::Root& root, gfx::KeyEventConsumer& keyHandler, afl::string::Translator& tx);
+        BaseTaskTile(Personality pers, ui::Root& root, gfx::KeyEventConsumer& keyHandler, afl::string::Translator& tx);
         ~BaseTaskTile();
 
         /** Set base prediction status.
@@ -36,6 +42,7 @@ namespace client { namespace tiles {
         void setBaseStatus(const game::proxy::TaskEditorProxy::BaseStatus& st);
 
      private:
+        Personality m_personality;
         ui::rich::DocumentView m_statusView;
         ui::widgets::Button m_editButton;
     };

P9/Settings
@@ -12,7 +12,9 @@ GENERATE =
 
 # Target definitions
 TARGETS += gamelib
-FILES_gamelib = game/interface/missionlistcontext.cpp \
+FILES_gamelib = game/interface/buildcommandparser.cpp \
+    game/interface/buildcommandparser.hpp \
+    game/interface/missionlistcontext.cpp \
     game/interface/missionlistcontext.hpp game/proxy/vcrexportadaptor.cpp \
     game/proxy/vcrexportadaptor.hpp game/proxy/viewpointstarbaseadaptor.cpp \
     game/proxy/viewpointstarbaseadaptor.hpp game/proxy/listproxy.cpp \
@@ -850,8 +852,7 @@ FILES_guilib = client/dialogs/missionselection.cpp \
     client/dialogs/shipcostcalculator.hpp client/dialogs/buildshiporder.cpp \
     client/dialogs/buildshiporder.hpp client/dialogs/buildshipmain.cpp \
     client/dialogs/buildshipmain.hpp client/dialogs/scriptcommanddialog.cpp \
-    client/dialogs/scriptcommanddialog.hpp client/tiles/planettasktile.cpp \
-    client/tiles/planettasktile.hpp client/tiles/basetasktile.cpp \
+    client/dialogs/scriptcommanddialog.hpp client/tiles/basetasktile.cpp \
     client/tiles/basetasktile.hpp client/tiles/shiptasktile.cpp \
     client/tiles/shiptasktile.hpp client/tiles/taskmessagetile.cpp \
     client/tiles/taskmessagetile.hpp client/map/shiptaskoverlay.cpp \
@@ -1797,7 +1798,8 @@ DEPEND_c2ng = guilib gamelib afl
 
 # Testsuite
 TARGETS += testsuite
-FILES_testsuite = test/game/interface/missionlistcontexttest.cpp \
+FILES_testsuite = test/game/interface/buildcommandparsertest.cpp \
+    test/game/interface/missionlistcontexttest.cpp \
     test/game/proxy/vcrexportadaptortest.cpp \
     test/server/talk/render/rendertest.cpp \
     test/game/proxy/viewpointstarbaseadaptortest.cpp \

client/tiles/planettasktile.cpp
@@ -1,24 +1,0 @@
-/**
-  *  \file client/tiles/planettasktile.cpp
-  *  \brief Class client::tiles::PlanetTaskTile
-  */
-
-#include "client/tiles/planettasktile.hpp"
-#include "ui/spacer.hpp"
-
-client::tiles::PlanetTaskTile::PlanetTaskTile(ui::Root& root, gfx::KeyEventConsumer& keyHandler, afl::string::Translator& tx)
-    : TaskMessageTile(root, keyHandler, tx)
-{
-    // ex WPlanetAutoTaskCommandTile::init
-    // statusPart() remains empty, but add a spacer so it consumes all remaining space
-    statusPart().add(deleter().addNew(new ui::Spacer()));
-
-    // Command buttons
-    addCommandButton('1', tx("1 - Orders"));
-    addCommandButton('2', tx("2 - Cargo"));
-    addCommandButton('3', tx("3 - Misc."));
-    commandPart().add(deleter().addNew(new ui::Spacer()));
-}
-
-client::tiles::PlanetTaskTile::~PlanetTaskTile()
-{ }

client/tiles/planettasktile.hpp
@@ -1,34 +1,0 @@
-/**
-  *  \file client/tiles/planettasktile.hpp
-  *  \brief Class client::tiles::PlanetTaskTile
-  */
-#ifndef C2NG_CLIENT_TILES_PLANETTASKTILE_HPP
-#define C2NG_CLIENT_TILES_PLANETTASKTILE_HPP
-
-#include "afl/string/translator.hpp"
-#include "client/tiles/taskmessagetile.hpp"
-#include "gfx/keyeventconsumer.hpp"
-#include "ui/root.hpp"
-
-namespace client { namespace tiles {
-
-    /** Planet task tile.
-        Displays commands to manipulate a planet task and notification message.
-        (For now, no prediction.)
-
-        To use,
-        - create
-        - connect TaskEditorProxy::sig_messageChange to setMessageStatus */
-    class PlanetTaskTile : public TaskMessageTile {
-     public:
-        /** Constructor.
-            @param root        UI root
-            @param keyHandler  Key handler to process button events
-            @param tx          Translator */
-        PlanetTaskTile(ui::Root& root, gfx::KeyEventConsumer& keyHandler, afl::string::Translator& tx);
-        ~PlanetTaskTile();
-    };
-
-} }
-
-#endif

client/tiles/tilefactory.cpp
@@ -12,7 +12,6 @@
 #include "client/tiles/basetasktile.hpp"
 #include "client/tiles/errortile.hpp"
 #include "client/tiles/planetscreenheadertile.hpp"
-#include "client/tiles/planettasktile.hpp"
 #include "client/tiles/selectionheadertile.hpp"
 #include "client/tiles/shipcargotile.hpp"
 #include "client/tiles/shipmovementtile.hpp"
@@ -597,15 +596,16 @@ client::tiles::TileFactory::createTile(String_t name, afl::base::Deleter& delete
         return &tile;
     }
     if (name == "PLANETTASKCOMMAND") {
-        PlanetTaskTile& tile = deleter.addNew(new PlanetTaskTile(root, m_keys, tx));
+        BaseTaskTile& tile = deleter.addNew(new BaseTaskTile(BaseTaskTile::PlanetPersonality, root, m_keys, tx));
         tile.setState(DisabledState, true); // FIXME: disable so it doesn't get focus - should we have a FocusableState instead?
         if (m_pTaskEditor != 0) {
-            m_pTaskEditor->sig_messageChange.add(&tile, &PlanetTaskTile::setMessageStatus);
+            m_pTaskEditor->sig_messageChange.add(&tile, &BaseTaskTile::setMessageStatus);
+            m_pTaskEditor->sig_baseChange.add(&tile, &BaseTaskTile::setBaseStatus);
         }
         return &tile;
     }
     if (name == "BASETASKCOMMAND") {
-        BaseTaskTile& tile = deleter.addNew(new BaseTaskTile(root, m_keys, tx));
+        BaseTaskTile& tile = deleter.addNew(new BaseTaskTile(BaseTaskTile::BasePersonality, root, m_keys, tx));
         tile.setState(DisabledState, true); // FIXME: disable so it doesn't get focus - should we have a FocusableState instead?
         if (m_pTaskEditor != 0) {
             m_pTaskEditor->sig_messageChange.add(&tile, &BaseTaskTile::setMessageStatus);

game/interface/buildcommandparser.cpp
@@ -1,0 +1,242 @@
+/**
+  *  \file game/interface/buildcommandparser.cpp
+  *  \brief Class game::interface::BuildCommandParser
+  */
+
+#include "game/interface/buildcommandparser.hpp"
+#include "afl/data/scalarvalue.hpp"
+#include "afl/string/format.hpp"
+#include "game/actions/buildparts.hpp"
+#include "game/actions/buildship.hpp"
+#include "game/actions/buildstarbase.hpp"
+#include "game/actions/buildstructures.hpp"
+#include "game/exception.hpp"
+#include "game/interface/planetmethod.hpp"
+#include "game/map/planetstorage.hpp"
+#include "interpreter/taskeditor.hpp"
+#include "interpreter/values.hpp"
+
+using afl::string::Format;
+using game::map::PlanetStorage;
+using game::actions::BuildParts;
+using game::actions::BuildShip;
+using game::actions::BuildStarbase;
+using game::actions::BuildStructures;
+
+namespace {
+    void renderAmount(afl::data::StringList_t& info, int n, int todo, int added, afl::string::Translator& tx)
+    {
+        // We might be half-way in a build action
+        if (todo != n) {
+            info.push_back(Format(tx("To build: %d/%d"), todo, n));
+        } else {
+            info.push_back(Format(tx("To build: %d"), n));
+        }
+
+        // - We might exceed supported amount (which means command will never finish)
+        if (added != todo) {
+            info.push_back(Format(tx("Only %d more supported!"), added));
+        }
+    }
+}
+
+game::interface::BuildCommandParser::BuildCommandParser(game::map::Planet& pl,
+                                                        game::spec::ShipList& shipList,
+                                                        Root& root,
+                                                        afl::string::Translator& tx)
+    : m_planet(pl),
+      m_shipList(shipList),
+      m_root(root),
+      m_translator(tx),
+      m_limit(),
+      m_result()
+{ }
+
+// Set limit for commands that build multiple items (e.g. "BuildFactoriesWait").
+void
+game::interface::BuildCommandParser::setLimit(int n)
+{
+    m_limit = n;
+}
+
+// Check task for applicable limit.
+void
+game::interface::BuildCommandParser::loadLimit(const interpreter::TaskEditor& editor, size_t pc)
+{
+    int v = 0;
+    if (editor.getPC() == pc && editor.isInSubroutineCall()) {
+        std::auto_ptr<afl::data::Value> value(editor.process().getVariable("BUILD.REMAINDER"));
+        if (afl::data::ScalarValue* sv = dynamic_cast<afl::data::ScalarValue*>(value.get())) {
+            v = sv->getValue();
+        }
+    }
+    setLimit(v);
+}
+
+// Get result.
+std::auto_ptr<game::interface::BuildCommandParser::Result>
+game::interface::BuildCommandParser::getResult()
+{
+    return m_result;
+}
+
+// TaskPredictor:
+bool
+game::interface::BuildCommandParser::predictInstruction(const String_t& name, interpreter::Arguments& args)
+{
+    try {
+        if (name == "BUILDSHIP" || name == "ENQUEUESHIP") {
+            handleBuildShip(args);
+        } else if (name == "BUILDBASE" || name == "BUILDBASEWAIT") {
+            handleBuildBase(args);
+        } else if (name == "BUILDDEFENSE" || name == "BUILDDEFENSEWAIT") {
+            handleBuildStructure(args, DefenseBuilding);
+        } else if (name == "BUILDFACTORIES" || name == "BUILDFACTORIESWAIT") {
+            handleBuildStructure(args, FactoryBuilding);
+        } else if (name == "BUILDBASEDEFENSE" || name == "BUILDBASEDEFENSEWAIT") {
+            handleBuildStructure(args, BaseDefenseBuilding);
+        } else if (name == "BUILDMINES" || name == "BUILDMINESWAIT") {
+            handleBuildStructure(args, MineBuilding);
+        } else if (name == "BUILDENGINES" || name == "BUILDENGINESWAIT") {
+            handleBuildParts(args, EngineTech);
+        } else if (name == "BUILDHULLS" || name == "BUILDHULLSWAIT") {
+            handleBuildParts(args, HullTech);
+        } else if (name == "BUILDBEAMS" || name == "BUILDBEAMSWAIT") {
+            handleBuildParts(args, BeamTech);
+        } else if (name == "BUILDLAUNCHERS" || name == "BUILDLAUNCHERSWAIT") {
+            handleBuildParts(args, TorpedoTech);
+        }
+    }
+    catch (Exception&) {
+        // TaskPredictor only guards against interpreter::Error; let's also guard against game::Exception.
+    }
+    return true;
+}
+
+void
+game::interface::BuildCommandParser::handleBuildShip(interpreter::Arguments& args)
+{
+    afl::base::Optional<ShipBuildOrder> order = parseBuildShipCommand(args, m_shipList);
+    if (const ShipBuildOrder* p = order.get()) {
+        // Action
+        PlanetStorage container(m_planet, m_root.hostConfiguration());
+        BuildShip action(m_planet, container, m_shipList, m_root);
+        action.setUsePartsFromStorage(false);
+        action.setBuildOrder(*p);
+
+        // Result
+        m_result.reset(new Result());
+        p->describe(m_result->info, m_shipList, m_translator);
+        m_result->cost = action.costAction().getCost();
+        m_result->missingAmount = action.costAction().getMissingAmountAsCost();
+        m_result->type = OrderType_Ship;
+    }
+}
+
+void
+game::interface::BuildCommandParser::handleBuildBase(interpreter::Arguments& args)
+{
+    // Only handle "BuildBase" or "BuildBase 1" commands (not "BuildBase 0")
+    args.checkArgumentCount(0, 1);
+    afl::data::Value* p = args.getNext();
+    if (p == 0 || interpreter::getBooleanValue(p) > 0) {
+        // Action
+        PlanetStorage container(m_planet, m_root.hostConfiguration());
+        BuildStarbase action(m_planet, container, true, m_root.hostConfiguration());
+
+        // Result
+        m_result.reset(new Result());
+        m_result->info.push_back(m_translator("Starbase"));
+        m_result->cost = action.costAction().getCost();
+        m_result->missingAmount = action.costAction().getMissingAmountAsCost();
+    }
+}
+
+void
+game::interface::BuildCommandParser::handleBuildStructure(interpreter::Arguments& args, PlanetaryBuilding type)
+{
+    args.checkArgumentCount(1);
+    int32_t n;
+    if (!interpreter::checkIntegerArg(n, args.getNext(), 0, MAX_NUMBER)) {
+        return;
+    }
+
+    const int todo = getLimitedAmount(n);;
+    if (todo > 0) {
+        // Action
+        PlanetStorage container(m_planet, m_root.hostConfiguration());
+        BuildStructures action(m_planet, container, m_root.hostConfiguration());
+
+        // Result
+        const int added = action.add(type, todo, true);
+        m_result.reset(new Result());
+        m_result->info.push_back(m_translator(BuildStructures::describe(type).untranslatedBuildingName));
+        renderAmount(m_result->info, n, todo, added, m_translator);
+
+        // Cost. Note that this will be partial cost if we're not supporting enough.
+        m_result->cost = action.costAction().getCost();
+        m_result->missingAmount = action.costAction().getMissingAmountAsCost();
+    }
+}
+
+void
+game::interface::BuildCommandParser::handleBuildParts(interpreter::Arguments& args, TechLevel area)
+{
+    // Parse args
+    // (Ignore arg #3, which is the optional "N" flag)
+    args.checkArgumentCount(2, 3);
+    int32_t type, amount;
+    if (!interpreter::checkIntegerArg(type, args.getNext(), 0, MAX_NUMBER)) {
+        return;
+    }
+    if (!interpreter::checkIntegerArg(amount, args.getNext(), -MAX_NUMBER, +MAX_NUMBER)) {
+        return;
+    }
+
+    // Validate
+    const game::spec::Component* comp = 0;
+    int slot = type;
+    switch (area) {
+     case HullTech:
+        slot = m_shipList.hullAssignments().getIndexFromHull(m_root.hostConfiguration(), m_planet.getOwner().orElse(0), type);
+        comp = m_shipList.hulls().get(type);
+        break;
+
+     case EngineTech:
+        comp = m_shipList.engines().get(type);
+        break;
+
+     case BeamTech:
+        comp = m_shipList.beams().get(type);
+        break;
+
+     case TorpedoTech:
+        comp = m_shipList.launchers().get(type);
+        break;
+    }
+
+    const int todo = getLimitedAmount(amount);
+    if (slot != 0 && comp != 0 && todo > 0) {
+        // Action
+        PlanetStorage container(m_planet, m_root.hostConfiguration());
+        BuildParts action(m_planet, container, m_shipList, m_root);
+
+        // Result
+        const int added = action.add(area, slot, todo, true);
+        m_result.reset(new Result());
+        m_result->info.push_back(comp->getName(m_shipList.componentNamer()));
+        renderAmount(m_result->info, amount, todo, added, m_translator);
+        m_result->cost = action.costAction().getCost();
+        m_result->missingAmount = action.costAction().getMissingAmountAsCost();
+    }
+}
+
+int
+game::interface::BuildCommandParser::getLimitedAmount(int requested) const
+{
+    if (m_limit > 0 && m_limit < requested) {
+        return m_limit;
+    } else {
+        return requested;
+    }
+}

game/interface/buildcommandparser.hpp
@@ -1,0 +1,108 @@
+/**
+  *  \file game/interface/buildcommandparser.hpp
+  *  \brief Class game::interface::BuildCommandParser
+  */
+#ifndef C2NG_GAME_INTERFACE_BUILDCOMMANDPARSER_HPP
+#define C2NG_GAME_INTERFACE_BUILDCOMMANDPARSER_HPP
+
+#include "afl/string/translator.hpp"
+#include "game/map/universe.hpp"
+#include "game/root.hpp"
+#include "game/spec/shiplist.hpp"
+#include "game/types.hpp"
+#include "interpreter/taskpredictor.hpp"
+
+namespace game { namespace interface {
+
+    /** Parser for "Build" commands.
+        This parses a single independant Build command and generates information about it.
+        Information includes a textual description of the order, costs, and missing amounts.
+        Supported commands include BuildShip, BuildBase, ship component building (BuildHulls etc.),
+        and structure building (BuildFactories etc.).
+
+        To use,
+        - create;
+        - call setLimit or loadLimit to support partially executed commands;
+        - call predictInstruction(), indirectly using TaskPredictor::predictStatement();
+        - extract result using getResult(). */
+    class BuildCommandParser : public interpreter::TaskPredictor {
+     public:
+        /** Type of order. */
+        enum OrderType {
+            OrderType_Other,
+            OrderType_Ship
+        };
+
+        /** Result. */
+        struct Result {
+            /** Textual information about things to be built.
+                In case of a ship, output of ShipBuildOrder::describe(). */
+            afl::data::StringList_t info;
+
+            /** Remaining cost. */
+            game::spec::Cost cost;
+
+            /** Missing resources required to complete command. */
+            game::spec::Cost missingAmount;
+
+            /** True if this is a ship build order. */
+            OrderType type;
+
+            Result()
+                : info(), cost(), missingAmount(), type(OrderType_Other)
+                { }
+        };
+
+        /** Constructor.
+            @param pl        Planet
+            @param shipList  Ship list (for validating parameters, costs)
+            @param root      Root (for configuration)
+            @param tx        Translator */
+        BuildCommandParser(game::map::Planet& pl,
+                           game::spec::ShipList& shipList,
+                           Root& root,
+                           afl::string::Translator& tx);
+
+        /** Set limit for commands that build multiple items (e.g.\ "BuildFactoriesWait").
+            If nonzero and in range, assumes that this command will build only so many structures
+            instead of the amount requested.
+
+            @param n Limit
+            @see loadLimit() */
+        void setLimit(int n);
+
+        /** Check task for applicable limit.
+            If the process is currently executing the given command,
+            retrieve the "Build.Remainder" variable.
+            This assumes that the command is implemented using a "Do .. While Build.Remainder" loop.
+
+            @param editor TaskEditor
+            @param pc     Program counter */
+        void loadLimit(const interpreter::TaskEditor& editor, size_t pc);
+
+        /** Get result.
+            @return result, if any. */
+        std::auto_ptr<Result> getResult();
+
+        // TaskPredictor:
+        virtual bool predictInstruction(const String_t& name, interpreter::Arguments& args);
+
+     private:
+        game::map::Planet& m_planet;
+        game::spec::ShipList& m_shipList;
+        Root& m_root;
+        afl::string::Translator& m_translator;
+        int m_limit;
+        std::auto_ptr<Result> m_result;
+
+        void handleBuildShip(interpreter::Arguments& args);
+        void handleBuildBase(interpreter::Arguments& args);
+        void handleBuildStructure(interpreter::Arguments& args, PlanetaryBuilding type);
+        void handleBuildParts(interpreter::Arguments& args, TechLevel area);
+
+        int getLimitedAmount(int requested) const;
+    };
+
+} }
+
+#endif

commit b4d928208de1eb7fc494e74ba152a832a767b5db

Base Task Tile: fix typo
Error introduced in 38726fba, causing "Edit" button to not be shown

client/tiles/basetasktile.cpp
@@ -26,7 +26,7 @@ client::tiles::BaseTaskTile::BaseTaskTile(Personality pers, ui::Root& root, gfx:
     addCommandButton('3', tx("3 - Misc."));
     commandPart().add(deleter().addNew(new ui::Spacer()));
 
-    if (m_personality == BasePersonality) {
+    if (pers == BasePersonality) {
         ui::Group& g = deleter().addNew(new ui::Group(ui::layout::HBox::instance5));
         g.add(deleter().addNew(new ui::widgets::StaticText(tx("Edit"), SkinColor::Static, gfx::FontRequest(), root.provider(), gfx::RightAlign))
               .setIsFlexible(true));

client/tiles/basetasktile.hpp
@@ -42,7 +42,6 @@ namespace client { namespace tiles {
         void setBaseStatus(const game::proxy::TaskEditorProxy::BaseStatus& st);
 
      private:
-        Personality m_personality;
         ui::rich::DocumentView m_statusView;
         ui::widgets::Button m_editButton;
     };